bb6bec8ede19a5d68dc5f8eb7137523652ad0744
Jay <99102582+ItheNerd@users.noreply.github.com>
2023-09-22 07:31:08 +0530

    added a modal

---
+++ src/data.js
@@ -0,0 +1,24 @@
+// data.js
+export const cardData = [
+  {
+    title: "Sothings / E-Commerce Website for Old Tech",
+    img: "../assets/post_img.webp",
+    desc: "Designed and developed a responsive and accessible e-commerce website for second-hand technology products using the MERN stack, Tailwind CSS, Radix UI, React Query, TypeScript, and Git.",
+    url: "https://github.com/ItheNerd/Sothing",
+    badge: "FOSS",
+  },
+  {
+    title: "MUJBento / Hostel Mess Timetable",
+    img: "../assets/post_img.webp",
+    desc: "Created an online hostel mess timetable application using NextUI, Tailwind CSS, React, Astro, and Git, with dynamic data sourced from Google Sheets for easy content management.",
+    url: "https://github.com/ItheNerd/FoodTimeTable",
+    badge: "Open Source",
+  },
+  {
+    title: "PrettyCodeSS / Code snippets and screenshots",
+    img: "../assets/post_img.webp",
+    desc: "A fully functional tool for sharing prettified screenshots of code snippets.",
+    url: "https://github.com/ItheNerd/PrettyCodeSS",
+    badge: "Open Source",
+  },
+];
